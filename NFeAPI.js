91c8d825cc6c7fafddd907fdc326ac3a59bf129e
Matheus <matheus.garcia@newssystems.eti.br>
2017-07-06 09:43:41 -0300

    Alterada organização de pastas

---
+++ NFeAPI.js
@@ -0,0 +1,41 @@
+function emitirNFe(token, nfe, tpConteudo){
+	var url = 'https://nfe.ns.eti.br/nfe/issue';
+    return enviaConteudoParaAPI(token, nfe, url, tpConteudo);
+}
+
+function consultarStatusProcessamento(token, CNPJ, nsNRec){
+	var json = '{"X-AUTH-TOKEN": "' + token + '", "CNPJ": "' + CNPJ + '", "nsNRec": "' + nsNRec + '"};'
+	var url = 'https://nfe.ns.eti.br/nfe/issue/status';
+	return enviaConteudoParaAPI(token, json, url, "json");
+}
+
+function downloadNFe(token, chNFe, tpDown){
+	var json = '{"X-AUTH-TOKEN": "' + token + '", "chNFe": "' + chNFe + '", "tpDown": "' + tpDown + '"};'
+	var url = 'https://nfe.ns.eti.br/nfe/get';
+	return enviaConteudoParaAPI(token, json, url, "json");
+}
+
+function enviaConteudoParaAPI(token, conteudo, urlEnvio, tpConteudo = "json"){
+	var content, retorno;
+	if(tpConteudo == "txt")	content = "text/plain";
+	else if(tpConteudo == "xml") content = "application/xml";
+	else content = "application/json";
+
+	$.ajax({
+		type: 'POST',
+		timeout: 10000,
+		async: false,
+		url: urlEnvio,
+		data: conteudo,
+        contentType: content,
+        headers: {
+            "X-AUTH-TOKEN": token
+        }
+    }).then(function(response){
+    	retorno = response;
+    }, function(response){
+        retorno = response;
+    });
+
+    return retorno;
+}
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# NFeApiClientJavaScript
-Exemplo de código de comunicação com a NS NFe API em JavaScript
-
-Os exemplos em Javascript foram construídos utilizando a biblioteca jQuery: https://jquery.com/
-
-Para mais informações acesse: http://confluence.ns.eti.br/display/PUB/NS+NF-e+API+-+Consumo+em+Javascript
+# NFeApiClientJavascript
+Exemplo de código de comunicação com a NS NFe API em JavaScript<br>
+No projeto existe o arquivo *'NFeAPI.js'*, onde foram implementadas as funções da API e este arquivo pode ser adicionado diretamente em seu sistema.<br>
+Para mais informações acesse: https://goo.gl/cQbrkN
